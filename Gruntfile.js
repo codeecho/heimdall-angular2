d636733fb5b2aa85a41a8a02526c69384d28e8fa
adamknight84 <adamknight84@gmail.com>
2016-09-14 20:17:21 +0000

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt) {
+
+  var merge = require("merge");
+
+  var includes = ["**/*"];
+
+  var config = {
+    pkg: grunt.file.readJSON("package.json"),
+    clean: ["build", "app/*", "dist"],
+    copy: {
+      stage: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: ["**/*"],
+          dest: "build/stage"
+        }]
+      },
+      publish: {
+        files: [{
+          expand: true,
+          cwd: "build/stage",
+          src: includes,
+          dest: "app"
+        }]
+      },
+    }
+  }
+
+  merge(config, {
+	express: {
+      all: {
+        options: {
+          port: 9000,
+          bases: ["app"],
+          livereload: true
+        }
+      }
+    },
+    open: {
+      all: {
+        path: 'http://localhost:<%= express.all.options.port%>'
+      }
+    },
+    watch: {
+      src: {
+        files: ['src/**/*', 'bower_components/**/*'],
+        tasks: ['copy:stage', "do-setup", "do-validate", "do-build", "copy:publish"]
+      }
+    }
+});
+//<humphrey:config:insert>//
+
+  grunt.initConfig(config);
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.registerTask("do-serve", ["build","express","open","watch"]);
+//<humphrey:subtask:insert>//
+
+  grunt.registerTask("do-setup", []);
+  grunt.registerTask("do-validate", []);
+  grunt.registerTask("do-build", []);
+  grunt.registerTask("do-test", []);
+  grunt.registerTask("do-package", []);
+  grunt.registerTask("do-archive", []);
+  grunt.registerTask("do-deploy", []);
+
+  grunt.registerTask("setup", ["clean", "copy:stage", "do-setup"]);
+  grunt.registerTask("validate", ["setup", "do-validate"]);
+  grunt.registerTask("build", ["validate", "do-build", "copy:publish"]);
+  grunt.registerTask("test", ["build", "do-test"]);
+  grunt.registerTask("package", ["test", "do-package"]);
+  grunt.registerTask("archive", ["package", "do-archive"]);
+  grunt.registerTask("deploy", ["archive", "do-deploy"]);
+
+  grunt.registerTask("serve", ["do-serve"]);
+//<humphrey:task:insert>//
+
+  grunt.registerTask("default", ["test"]);
+
+};
